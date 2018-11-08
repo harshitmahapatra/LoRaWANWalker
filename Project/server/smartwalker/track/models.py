5ff0473cff56a6b0b3a555c2e740d346b4bd342e
Harshit Mahapatra <harshitmahapatra95@gmail.com>
2018-11-08 15:26:55 +0100

    Server works

--- Project/server/smartwalker/track/models.py
+++ Project/server/smartwalker/track/models.py
@@ -5,10 +5,11 @@ class SensorData(models.Model):
     celcius = models.DecimalField(max_digits=10,decimal_places=7)
     humidity = models.DecimalField(max_digits=10,decimal_places=7)
     pressure = models.IntegerField(default=0)
+    movement = models.BooleanField(default=False)
     
     def __str__(self):
-        summary = "Celcius: " + str(self.celcius) + "Humidity: " + str(self.humidity) + "Pressure: " + str(self.pressure) 
+        summary = "Celcius: " + str(self.celcius) + " Humidity: " + str(self.humidity) + " Pressure: " + str(self.pressure) + " Movement: " + str(self.movement) + "\n"
         return summary
     def getsummary(self):
-        summary = "Celcius: " + str(self.celcius) + "Humidity: " + str(self.humidity) + "Pressure: " + str(self.pressure) 
+        summary = "Celcius: " + str(self.celcius) + " Humidity: " + str(self.humidity) + " Pressure: " + str(self.pressure) + " Movement: " + str(self.movement) + "\n"
         return summary
--- Project/server/smartwalker/track/mqtt.py
+++ Project/server/smartwalker/track/mqtt.py
@@ -3,7 +3,7 @@ from .deserializer import Payload
 from .models import SensorData
 APPID  = "56470000001"
 PW    = "ttn-account-v2.oi0FW4p2vZlZgXx1hutQzHfE8EfU9HTeSMY89cEfONc"
-SensorData.objects.all().delete()
+#SensorData.objects.all().delete()
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -11,17 +11,17 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    #client.subscribe('+/devices/+/up')
-    client.subscribe('test')
+    client.subscribe('+/devices/+/up')
+    #client.subscribe('test')
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
     full_json = Payload(msg.payload)
     #print(full_json["payload_fields"])
-    data = full_json.__dict__["payload_fields"]
-    sd = SensorData(celcius = data["celcius"],humidity = data["humidity"], pressure = data["pressure"])
-    sd.save()
+    #data = full_json.__dict__["payload_fields"]
+    #sd = SensorData(celcius = data["celcius"],humidity = data["humidity"], pressure = data["pressure"], movement = bool(data["m"]))
+    #sd.save()
     print("succesfull!!")
     
 def on_disconnect(client, userdata, rc):
@@ -35,8 +35,8 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_disconnect = on_disconnect
 client.on_message = on_message
-#client.username_pw_set(APPID,PW)
-#client.connect("eu.thethings.network", 1883, 60)
-client.connect("broker.mqttdashboard.com",1883,60)
+client.username_pw_set(APPID,PW)
+client.connect("eu.thethings.network", 1883, 60)
+#client.connect("broker.mqttdashboard.com",1883,60)
 
 
