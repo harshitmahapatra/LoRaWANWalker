3364a292f477c503548259a0e7b61c9ba92a0558
Patrick Lewandowski <au614714@uni.au.dk>
2018-11-26 12:00:00 +0100

    Pulse

--- Project/node/lorawan-arduino-bme/lib/Pulse/Pulse.cpp
+++ Project/node/lorawan-arduino-bme/lib/Pulse/Pulse.cpp
@@ -1,37 +1,45 @@
 #include <Arduino.h>
 
 
-int BPM, avgValue, LastTime, InitTime;
+int BPM, avgValue, LastTime, InitTime, pastValue;
 
 int GetAvgHR(int8_t SIGNAL_PIN)
 {
     LastTime = millis();
-    avgValue = 550;
+    avgValue = 450;
+    pastValue =  analogRead(SIGNAL_PIN);
+    int signalRead=0;
     BPM=0;
-    while(!(60<BPM && BPM<120))
+    while(!(55<BPM && BPM<150))
     {
-        //Serial.println(avgValue);
-        //Serial.println(analogRead(0));
+        signalRead = analogRead(SIGNAL_PIN);
+        
+        avgValue = avgValue * 0.92 + signalRead * 0.08;
 
-        avgValue = avgValue * 0.95 + analogRead(SIGNAL_PIN) * 0.05;
-        //difference=analogRead(0)/avgValue;
-        //Serial.println(difference);
-
-        if (analogRead(SIGNAL_PIN) > (avgValue + 70) && analogRead(SIGNAL_PIN) < (avgValue + 100) && avgValue > 430 && avgValue < 830)
+        // Print for visualization
+        // Serial.println(signalRead - avgValue);
+        // Serial.print(" ");
+        // Serial.print(signalRead);
+        // Serial.print(" ");
+        // Serial.print(avgValue);
+        // Serial.print(" ");
+        // if(pastValue>signalRead)
+        // {
+        //     Serial.println(400);
+        // }else{
+        //     Serial.println(420);
+        // }
+        // Serial.println("");
+        
+        if (pastValue>signalRead&&signalRead>(avgValue+20))
         {
             BPM = millis() - LastTime;
             BPM = int(60 / (float(BPM) / 1000));
             LastTime = millis();
-            //Serial.print(avgValue);
-            //Serial.print("    ");
-            //Serial.print(analogRead(0));
-            //Serial.print("    ");
-            //Serial.print(analogRead(0)-avgValue);
-            //Serial.print("    ");
-            //Serial.println(BPM);
-            delay(400);
         }
-        delay(5);
+        pastValue =  signalRead;
+        delay(10);
+      
     }
     return BPM;
 }
